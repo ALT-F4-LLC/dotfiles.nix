75dfac51985f891197ce60550434ff88d181fc91
Erik Reinert <erik@altf4.email>
2022-06-03 11:38:31 -0700

    refactor(home-manager): shared sessionVariables

--- users/ereinert/home-manager.nix
+++ users/ereinert/home-manager.nix
@@ -24,15 +24,4 @@ in {
       pkgs.slack
     ];
   };
-
-  #---------------------------------------------------------------------
-  # Env vars and dotfiles
-  #---------------------------------------------------------------------
-
-  home.sessionVariables = {
-    LANG = "en_US.UTF-8";
-    LC_CTYPE = "en_US.UTF-8";
-    LC_ALL = "en_US.UTF-8";
-    EDITOR = "nvim";
-  };
 }
--- users/erikreinert/home-manager.nix
+++ users/erikreinert/home-manager.nix
@@ -17,15 +17,4 @@ in {
       pkgs.discord
     ];
   };
-
-  #---------------------------------------------------------------------
-  # Env vars and dotfiles
-  #---------------------------------------------------------------------
-
-  home.sessionVariables = {
-    LANG = "en_US.UTF-8";
-    LC_CTYPE = "en_US.UTF-8";
-    LC_ALL = "en_US.UTF-8";
-    EDITOR = "nvim";
-  };
 }
