db6d779f6a53dbb96fd7cd656bed36aa03ad4995
Erik Reinert <erik@altf4.email>
2022-06-03 12:27:53 -0700

    feat(shared): add nodejs and python

--- users/shared/home-packages.nix
+++ users/shared/home-packages.nix
@@ -8,25 +8,27 @@
   pkgs.firefox-bin
   pkgs.gcc
   pkgs.git-secret
+  pkgs.jetbrains.datagrip
+  pkgs.jetbrains.jdk
   pkgs.k9s
   pkgs.kind
   pkgs.lazydocker
   pkgs.lazygit
   pkgs.nnn
-  pkgs.jetbrains.jdk
-  pkgs.jetbrains.datagrip
+  pkgs.nodejs-16_x
+  pkgs.python3Full
 
   # language servers
-  pkgs.nodePackages."bash-language-server"
-  pkgs.nodePackages."vscode-langservers-extracted"
-  pkgs.nodePackages."dockerfile-language-server-nodejs"
   pkgs.elixir_ls
   pkgs.gopls
+  pkgs.nodePackages."bash-language-server"
+  pkgs.nodePackages."dockerfile-language-server-nodejs"
+  pkgs.nodePackages."typescript"
+  pkgs.nodePackages."typescript-language-server"
+  pkgs.nodePackages."vscode-langservers-extracted"
+  pkgs.nodePackages."yaml-language-server"
   pkgs.python3Packages."python-lsp-server"
   pkgs.rust-analyzer
   pkgs.sumneko-lua-language-server
   pkgs.terraform-lsp
-  pkgs.nodePackages."typescript"
-  pkgs.nodePackages."typescript-language-server"
-  pkgs.nodePackages."yaml-language-server"
 ] ++ extras
