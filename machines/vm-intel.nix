218c84a316f24755c44f83fff4a3f1f696f94202
Erik Reinert <erik@altf4.email>
2022-06-02 20:02:24 -0700

    feat(vm-intel): add shared folder support

--- machines/vm-intel.nix
+++ machines/vm-intel.nix
@@ -8,4 +8,18 @@
 
   # Interface is this on Intel Fusion
   networking.interfaces.ens33.useDHCP = true;
+
+  # Shared folder to host works on Intel
+  fileSystems."/host" = {
+    fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
+    device = ".host:/";
+    options = [
+      "umask=22"
+      "uid=1000"
+      "gid=1000"
+      "allow_other"
+      "auto_unmount"
+      "defaults"
+    ];
+  };
 }
