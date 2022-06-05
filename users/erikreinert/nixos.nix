a9e1b7deab3b3c37b372dec219b9f74cca685faf
Erik Reinert <erik@altf4.email>
2022-06-05 15:05:24 -0700

    feat(erikreinert): add ipad public key

--- users/erikreinert/nixos.nix
+++ users/erikreinert/nixos.nix
@@ -7,7 +7,9 @@
     extraGroups = [ "audio" "docker" "wheel" ];
     hashedPassword = "";
     shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJs7Z5a/QPZPaly3N79Ns4qL73k9XMACmqH8H03gHMXf Erik-Reinert-iPad"
+    ];
   };
 
   nixpkgs.overlays = import ../../lib/overlays.nix ++ [
