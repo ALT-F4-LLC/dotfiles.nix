526f3a2ea4fe9f1c872972c13e065b5256787342
Erik Reinert <erik@altf4.email>
2022-06-14 12:33:53 -0700

    feat(nix): add gc config

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -5,12 +5,17 @@
 
   nix = {
     autoOptimiseStore = true;
-    package = pkgs.nixUnstable;
     extraOptions = ''
       experimental-features = nix-command flakes
       keep-outputs = true
       keep-derivations = true
     '';
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+    package = pkgs.nixUnstable;
   };
 
   nixpkgs.config.allowUnfree = true;
