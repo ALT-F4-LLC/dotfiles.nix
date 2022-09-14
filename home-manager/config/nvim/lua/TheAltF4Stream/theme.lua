5d42a050065cb884781679a08551b943e1f00d5c
Erik Reinert <erik@altf4.email>
2022-09-14 13:56:34 -0700

    feat(catppuccin): added integrations

--- home-manager/config/nvim/lua/TheAltF4Stream/theme.lua
+++ home-manager/config/nvim/lua/TheAltF4Stream/theme.lua
@@ -4,10 +4,31 @@ local function setup_theme()
 
   -- Load colorscheme
   require("catppuccin").setup({
+    integrations = {
+      indent_blankline = {
+        enabled = true,
+        colored_indent_levels = true,
+      },
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+        },
+      },
+    },
     transparent_background = true,
   })
 
-  vim.cmd[[colorscheme catppuccin]]
+  vim.cmd [[colorscheme catppuccin]]
 end
 
 local function init()
