41bc620cf71bd5499c6c5e7dc00ec4f7d49fceb1
Erik Reinert <erik@altf4.email>
2022-10-20 18:55:33 -0700

    chore(update): flake + niv

--- home-manager/config/nvim/lua/TheAltF4Stream/languages.lua
+++ home-manager/config/nvim/lua/TheAltF4Stream/languages.lua
@@ -40,7 +40,7 @@ local function on_attach(client, bufnr)
 end
 
 local function make_config()
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   capabilities.textDocument.completion.completionItem.resolveSupport = {
