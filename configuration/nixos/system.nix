42a1fa0ebdec87c0ee08c20a0a993bed3de389f6
Erik Reinert <erik@altf4.email>
2022-12-16 00:40:15 -0800

    chore(update): nixos packages

--- configuration/nixos/system.nix
+++ configuration/nixos/system.nix
@@ -1,6 +1,4 @@
 { config, pkgs, ... }: {
-  imports = [ ../../nix/services/networking/twingate.nix ];
-
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
