904e254f5ee81ac074e4e9870d978099f655fc6b
Erik Reinert <erik@altf4.email>
2023-02-12 12:55:10 -0800

    fix(flake): remove patch for neovim nightly

--- flake.nix
+++ flake.nix
@@ -4,11 +4,7 @@
     darwin.url = "github:lnl7/nix-darwin";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
-    neovim-nightly = {
-      # workaround: https://github.com/nix-community/neovim-nightly-overlay/issues/164
-      inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
-      url = "github:nix-community/neovim-nightly-overlay";
-    };
+    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
