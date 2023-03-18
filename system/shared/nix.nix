17a3ba0059ecaa77ae7afcb802f2c984c49ad51d
Erik Reinert <erik@altf4.email>
2023-03-18 16:17:48 -0700

    feat(nix): moves extraOptions to settings

--- system/shared/nix.nix
+++ system/shared/nix.nix
@@ -1,26 +1,17 @@
 { pkgs }: {
-  extraOptions = ''
-    experimental-features = nix-command flakes
-    keep-derivations = true
-    keep-outputs = true
-    trusted-users = erikreinert
-    warn-dirty = false
-  '';
-
-  gc = {
-    automatic = true;
-    options = "--delete-older-than 7d";
-  };
-
   package = pkgs.nixUnstable;
 
   settings = {
     auto-optimise-store = true;
+    builders-use-substitutes = true;
+    experimental-features = [ "nix-command" "flakes" ];
     substituters = [
       "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    trusted-users = [ "@wheel" ];
+    warn-dirty = false;
   };
 }
