5de533dbd89779f099e33555b4dbdcf36aa05056
Erik Reinert <erik@altf4.email>
2023-05-20 11:47:25 -0700

    feat(nixos): use secondary drive for store

--- system/nixos/configuration-desktop.nix
+++ system/nixos/configuration-desktop.nix
@@ -17,6 +17,13 @@
     ];
   };
 
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-label/nix";
+    fsType = "ext4";
+    neededForBoot = true;
+    options = [ "noatime" ];
+  };
+
   hardware = {
     opengl.enable = true;
 
