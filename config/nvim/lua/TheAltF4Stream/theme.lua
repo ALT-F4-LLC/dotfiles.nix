15a976367c521e4800aaedfe9fe624e1536bc75d
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-06-15 03:27:24 -0700

    feat(neovim): added support for noice with lualine

--- config/nvim/lua/TheAltF4Stream/theme.lua
+++ config/nvim/lua/TheAltF4Stream/theme.lua
@@ -2,6 +2,7 @@ local catppuccin = require 'catppuccin'
 local colorizer = require 'colorizer'
 local gitsigns = require 'gitsigns'
 local lualine = require 'lualine'
+local noice = require 'noice'
 
 local function init()
     catppuccin.setup({
@@ -29,6 +30,29 @@ local function init()
             icons_enabled = false,
             extensions = { "fzf", "quickfix" },
             theme = "catppuccin"
+        },
+        sections = {
+            lualine_x = {
+                {
+                    noice.api.status.message.get_hl,
+                    cond = noice.api.status.message.has,
+                },
+                {
+                    noice.api.status.command.get,
+                    cond = noice.api.status.command.has,
+                    color = { fg = "#ff9e64" },
+                },
+                {
+                    noice.api.status.mode.get,
+                    cond = noice.api.status.mode.has,
+                    color = { fg = "#ff9e64" },
+                },
+                {
+                    noice.api.status.search.get,
+                    cond = noice.api.status.search.has,
+                    color = { fg = "#ff9e64" },
+                },
+            },
         }
     }
 
