afdfb1438d30fae78a6fc8fe0420e9d6407f5cad
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-06-21 20:14:31 -0700

    fix(neovim): re-added noice setting

--- config/nvim/lua/TheAltF4Stream/noice.lua
+++ config/nvim/lua/TheAltF4Stream/noice.lua
@@ -17,6 +17,7 @@ local function init()
             override = {
                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                 ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
             },
         },
         popupmenu = {
