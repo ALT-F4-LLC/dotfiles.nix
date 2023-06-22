0a122a94f0b73c8b43df442d27767c32f6320b3e
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-06-21 20:04:09 -0700

    chore(format)

--- system/shared/home-manager-packages.nix
+++ system/shared/home-manager-packages.nix
@@ -1,21 +1,21 @@
 { pkgs, ... }:
 
 with pkgs; [
-    awscli2
-    azure-cli
-    cachix
-    doppler
-    fd
-    gh
-    google-cloud-sdk
-    jq
-    just
-    k9s
-    kubectl
-    lazydocker
-    powershell
-    pulumi-bin
-    ripgrep
-    terraform
-    viddy
+  awscli2
+  azure-cli
+  cachix
+  doppler
+  fd
+  gh
+  google-cloud-sdk
+  jq
+  just
+  k9s
+  kubectl
+  lazydocker
+  powershell
+  pulumi-bin
+  ripgrep
+  terraform
+  viddy
 ]
