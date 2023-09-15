1f09cab60ef211627658f1b3eeee09cabbcd2642
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-09-15 10:52:33 -0700

    chore(clean)

--- flake.nix
+++ flake.nix
@@ -21,10 +21,6 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "aarch64-darwin" "x86_64-linux" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devShells = {
-          default = pkgs.mkShell { };
-        };
-
         packages = {
           thealtf4stream-nvim = pkgs.vimUtils.buildVimPlugin {
             name = "TheAltF4Stream";
