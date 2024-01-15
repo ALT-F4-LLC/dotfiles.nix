38625cdb1417defcacd07bd9a449f673bae059cc
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-01-15 09:04:36 -0800

    fix(flake): remove extra profiles

--- flake.nix
+++ flake.nix
@@ -20,13 +20,11 @@
       flake = {
         darwinConfigurations = {
           aarch64 = mkDarwin { system = "aarch64-darwin"; };
-          x86_64 = mkDarwin { system = "x86_64-darwin"; };
         };
 
         lib = import ./lib { inherit inputs; };
 
         nixosConfigurations = {
-          aarch64 = mkNixos { system = "aarch64-linux"; };
           x86_64 = mkNixos { system = "x86_64-linux"; };
         };
       };
