748afb20b530a85d893a0a71c6f2a53e0205b104
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-03-19 22:33:25 -0700

    feat: update bat theme to tokyonight and adjust kitty and lazygit configurations
    - Change bat theme from catppuccin to tokyonight.
    - Update theme source to folke/tokyonight.nvim.
    - Adjust kitty font size and remove specific color settings for a cleaner configuration.
    - Introduce Tokyo Night theme setting in kitty.
    - Add new alias `sgpt` to lazygit configuration.

--- lib/shared/home-manager.nix
+++ lib/shared/home-manager.nix
@@ -61,18 +61,18 @@ in {
 
   programs.bat = {
     enable = true;
-    config = {theme = "catppuccin";};
+    config = {theme = "tokyonight";};
     themes = {
-      catppuccin = {
+      tokyonight = {
         src =
           pkgs.fetchFromGitHub
           {
-            owner = "catppuccin";
-            repo = "bat";
-            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-            sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+            owner = "folke";
+            repo = "tokyonight.nvim";
+            rev = "c025baf23b62f044eff1f4ef561c45de636f0e32";
+            sha256 = "sha256-wG4rQ4o8Q1wcsQvha/4EKRvQ1rvguU5W34h7JMSKpek=";
           };
-        file = "Catppuccin-macchiato.tmTheme";
+        file = "extras/sublime/tokyonight_night.tmTheme";
       };
     };
   };
@@ -154,53 +154,24 @@ in {
       package = inputs.self.packages.${pkgs.system}.geist-mono;
       size =
         if isDarwin
-        then 18
-        else 15;
+        then 20
+        else 18;
     };
 
     settings = {
-      active_border_color = "#ee5396";
-      active_tab_background = "#ee5396";
-      active_tab_foreground = "#161616";
       allow_remote_control = "yes";
-      background = "#161616";
       background_opacity = "0.9";
-      bell_border_color = "#ee5396";
-      color0 = "#262626";
-      color1 = "#ff7eb6";
-      color10 = "#42be65";
-      color11 = "#82cfff";
-      color12 = "#33b1ff";
-      color13 = "#ee5396";
-      color14 = "#3ddbd9";
-      color15 = "#ffffff";
-      color2 = "#42be65";
-      color3 = "#82cfff";
-      color4 = "#33b1ff";
-      color5 = "#ee5396";
-      color6 = "#3ddbd9";
-      color7 = "#dde1e6";
-      color8 = "#393939";
-      color9 = "#ff7eb6";
-      cursor = "#f2f4f8";
-      cursor_text_color = "#393939";
       enabled_layouts = "splits";
-      foreground = "#dde1e6";
       hide_window_decorations = "titlebar-and-corners";
-      inactive_border_color = "#ff7eb6";
-      inactive_tab_background = "#393939";
-      inactive_tab_foreground = "#dde1e6";
       listen_on = "unix:/tmp/kitty";
       macos_option_as_alt = "yes";
       macos_quit_when_last_window_closed = "yes";
       macos_titlebar_color = "system";
-      selection_background = "#525252";
-      selection_foreground = "#f2f4f8";
-      tab_bar_background = "#161616";
-      url_color = "#ee5396";
       url_style = "single";
       wayland_titlebar_color = "system";
     };
+
+    theme = "Tokyo Night";
   };
 
   programs.lazygit = {
@@ -333,6 +304,7 @@ in {
         else "n -P K";
       nb = "nix build --json --no-link --print-build-logs";
       s = ''doppler run --config "nixos" --project "$(whoami)"'';
+      sgpt = "s -- sgpt --no-cache";
       wt = "git worktree";
     };
 
